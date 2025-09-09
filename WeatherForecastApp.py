57954ebf7459106353c2001ace76c035f4ea9372
te1u-lite <mercury7400hgn@gmail.com>
2025-09-09 14:38:35 +0900

    毎朝7時にSlackで自動通知されるように

--- WeatherForecastApp.py
+++ WeatherForecastApp.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # APIキー
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 if not API_KEY:
     raise ValueError("環境変数 'API_KEY' が設定されていません。")
@@ -28,7 +28,21 @@ if response.status_code == 200:
     current_temp = data["main"]["temp"]
 
     # 今日の気温
-    print(f"現在の{city}の天気: {current_weather}")
-    print(f"現在の気温: {current_temp}℃")
-else:
-    print("エラー:", data)
+#     print(f"現在の{city}の天気: {current_weather}")
+#     print(f"現在の気温: {current_temp}℃")
+# else:
+#     print("エラー:", data)
+
+
+#SlackAPI
+TOKEN = os.getenv("SLACK_TOKEN")
+CHANNEL = os.getenv("SLACK_CHANNEL")
+
+url = "https://slack.com/api/chat.postMessage"
+headers = {"Authorization": "Bearer "+TOKEN}
+data = {
+    "channel": CHANNEL,
+    "text": f"現在の{city}の天気: {current_weather}"+"\n"+f"現在の気温: {current_temp}℃"
+}
+r =requests.post(url,headers=headers,data=data)
+#print("return",r.json())
